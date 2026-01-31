22d1aa1de8c1be0c1cce74e4c83b9c35150c288a
Karanjjfhfhfhf <130735144+Karanjjfhfhfhf@users.noreply.github.com>
2026-01-31 12:10:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const reveals=document.querySelectorAll(".reveal");
-window.addEventListener("scroll",()=>{
-  reveals.forEach(el=>{
-    if(el.getBoundingClientRect().top<window.innerHeight-100){
-      el.classList.add("active");
-    }
-  });
-});
